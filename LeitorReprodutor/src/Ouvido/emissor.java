d2b2476781146d9cb3bdbcfe11604e60997df704
José Jorge <jorginhoajp2@gmail.com>
2025-06-13 00:27:54 -0300

    Desenvolvendo um tipo de bot de criação de legenda para o discord e site
    Primeiro passos para desenvolver as primeiras linhas de programações, de um acessibilidade para pessoas que tem problemas de audição
    --------------------------------
    First steps to develop the first lines of programming, accessibility for people with hearing impairments

---
+++ LeitorReprodutor/src/Ouvido/emissor.java
@@ -0,0 +1,11 @@
+package Ouvido;
+
+public class emissor {
+
+	public static void main(String[] args) {
+		
+		// Por hora vazio por causa do diagrama incompleto
+
+	}
+
+}
---
+++ LeitorReprodutor/src/Ouvido/receptor.java
@@ -0,0 +1,11 @@
+package Ouvido;
+
+public class receptor {
+
+	public static void main(String[] args) {
+		
+		// Por hora vazio por causa do diagrama incompleto
+
+	}
+
+}
